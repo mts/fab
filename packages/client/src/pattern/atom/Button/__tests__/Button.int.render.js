08f9e11924f87ea81785bd8c70394b3129c6c2d4
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-23 22:10:05 +0100

    implement Button onClick()

--- packages/client/src/pattern/atom/Button/__tests__/Button.int.render.js
+++ packages/client/src/pattern/atom/Button/__tests__/Button.int.render.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { Button } from '../Button'
+import { getAppMockStore } from '../../../../../../library/src/store/mock'
+import { mockAppState } from '../../../../../../library/src/state/mock'
 
-const doNothing = () => {}
-
-export const defaultRender = <Button text="Find" onClick={doNothing} />
+export const defaultRender = (
+  <Provider store={getAppMockStore(mockAppState)}>
+    <Button text="Find" />
+  </Provider>
+)
--- packages/client/src/pattern/atom/Button/__tests__/Button.int.test.js
+++ packages/client/src/pattern/atom/Button/__tests__/Button.int.test.js
@@ -1,5 +1,7 @@
 import { defaultRender } from './Button.int.render'
 
+jest.mock('../../../../store/thunk/completeState', () => ({ setAppCompleteStateThunk: () => {} }))
+
 describe('<Button />', () => {
   describe('Snaphot', () => {
     test('must match defaultRender', () => {

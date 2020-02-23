003276ec3215de9aec3d1ebec03feb0b16d37785
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-23 20:37:20 +0100

    implement HomeTemplate

--- packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
+++ packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import cx from 'classnames'
 import { homeTemplate } from './HomeTemplate.scss'
 import { homeTemplateDefaultProps, homeTemplatePropTypes } from './HomeTemplate.prop'
+import { FindDogBreed } from '../../organism/FindDogBreed'
 
 export function HomeTemplate({ className }) {
-  return <div className={cx(className, homeTemplate)}>HomeTemplate under construction</div>
+  return (
+    <div className={cx(className, homeTemplate)}>
+      <FindDogBreed />
+    </div>
+  )
 }
 
 HomeTemplate.defaultProps = homeTemplateDefaultProps

251d44460852745f99a5373c123a5b2a8d75cb1d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-23 20:36:43 +0100

    implement FindDogBreed

--- packages/client/src/pattern/organism/FindDogBreed/FindDogBreed.jsx
+++ packages/client/src/pattern/organism/FindDogBreed/FindDogBreed.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { findDogBreedDefaultProps, findDogBreedPropTypes } from './FindDogBreed.prop'
+import { FindAnimalBreed } from '../../molecule/FindAnimalBreed'
+import { dogSelectOptions } from '../../atom/Select/__tests__/Select.int.render'
 
-export function FindDogBreed({ className }) {
-  return <div className={className}>FindDogBreed under construction</div>
+export function Component({ className, randomImage }) {
+  const searchDogButtonOnClick = () => {}
+
+  return (
+    <div className={className}>
+      <FindAnimalBreed
+        selectOptions={dogSelectOptions}
+        buttonText="Search Dog"
+        buttonOnClick={searchDogButtonOnClick}
+        imageSource={{ src: randomImage.src, alt: randomImage.alt }}
+      />
+    </div>
+  )
 }
 
-FindDogBreed.defaultProps = findDogBreedDefaultProps
+Component.defaultProps = findDogBreedDefaultProps
 
-FindDogBreed.propTypes = findDogBreedPropTypes
+Component.propTypes = findDogBreedPropTypes
+
+export const FindDogBreed = connect(
+  ({ api }) => ({
+    randomImage: api.rest.dog.randomImage,
+  }),
+  dispatch => bindActionCreators({}, dispatch),
+)(Component)

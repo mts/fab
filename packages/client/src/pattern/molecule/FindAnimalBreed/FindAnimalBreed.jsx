f588d1781e2b0eabaf8363e66d350a1c0ae5f260
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-23 13:45:52 +0100

    implement breed search

--- packages/client/src/pattern/molecule/FindAnimalBreed/FindAnimalBreed.jsx
+++ packages/client/src/pattern/molecule/FindAnimalBreed/FindAnimalBreed.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import cx from 'classnames'
 import { findAnimalBreedDefaultProps, findAnimalBreedPropTypes } from './FindAnimalBreed.prop'
-import { breed } from './FindAnimalBreed.scss'
-import { Select } from '../../atom/Select/Select'
+import { breed, breedSearch, breedSearchSelect, breedSearchButton } from './FindAnimalBreed.scss'
+import { Select } from '../../atom/Select'
+import { Button } from '../../atom/Button'
 
-export function FindAnimalBreed({ className, selectOptions }) {
+export function FindAnimalBreed({ className, selectOptions, buttonText, buttonOnClick }) {
   return (
     <div className={cx(className, breed)}>
-      <Select options={selectOptions} />
+      <div className={breedSearch}>
+        <Select className={breedSearchSelect} options={selectOptions} />
+        <Button className={breedSearchButton} text={buttonText} onClick={buttonOnClick} />
+      </div>
     </div>
   )
 }

a3112fffd683bf67a0ff6a3614264f6274d3e9e9
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-23 14:17:44 +0100

    test breed show

--- packages/client/src/pattern/molecule/FindAnimalBreed/__tests__/FindAnimalBreed.int.render.js
+++ packages/client/src/pattern/molecule/FindAnimalBreed/__tests__/FindAnimalBreed.int.render.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { FindAnimalBreed } from '../FindAnimalBreed'
 import { dogSelectOptions } from '../../../atom/Select/__tests__/Select.int.render'
+import { imageSource } from '../../../atom/Image/__tests__/Image.int.render'
 
 const searchDogButtonOnClick = () => {}
 
 export const findDogBreedRender = (
-  <FindAnimalBreed selectOptions={dogSelectOptions} buttonText="Search Dog" buttonOnClick={searchDogButtonOnClick} />
+  <FindAnimalBreed
+    selectOptions={dogSelectOptions}
+    buttonText="Search Dog"
+    buttonOnClick={searchDogButtonOnClick}
+    imageSource={imageSource}
+  />
 )

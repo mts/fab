7066bccdc89abafec24f985fc9dc022333595989
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-23 20:36:00 +0100

    implement setAppCompleteStateThunk()

--- packages/client/src/store/thunk/completeState.js
+++ packages/client/src/store/thunk/completeState.js
@@ -1,3 +1,8 @@
-/* eslint-disable */
-export const setAppCompleteStateThunk = () => async dispatch => {}
-/* eslint-enable */
+import { apiRestDogSetRandomImageAction } from '../action/actionCreators'
+import { getRandomDogImageByBreed } from '../../api/rest/dog/image'
+import { dogSelectOptions } from '../../pattern/atom/Select/__tests__/Select.int.render'
+
+export const setAppCompleteStateThunk = () => async dispatch => {
+  const restResponseData = await getRandomDogImageByBreed(dogSelectOptions[0].value)
+  dispatch(apiRestDogSetRandomImageAction(restResponseData))
+}

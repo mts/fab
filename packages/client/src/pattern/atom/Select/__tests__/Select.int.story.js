738ad788fa0bb8128f627edb74b75e96287707ee
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-23 14:53:05 +0100

    render Select for dog and cat

--- packages/client/src/pattern/atom/Select/__tests__/Select.int.story.js
+++ packages/client/src/pattern/atom/Select/__tests__/Select.int.story.js
@@ -1,7 +1,10 @@
 import { storiesOf } from '@storybook/react'
-import { dogSelectRender } from './Select.int.render'
+import { dogSelectRender, catSelectRender } from './Select.int.render'
 import { getInfo } from '../../../../../../../.storybook/library'
 
-const dogSelectRenderInfo = getInfo('default')
+const dogSelectRenderInfo = getInfo('dogSelect')
+const catSelectRenderInfo = getInfo('catSelect')
 
-storiesOf('Patttern/Atom/Select', module).add(dogSelectRenderInfo.text, () => dogSelectRender, dogSelectRenderInfo.parameters)
+storiesOf('Patttern/Atom/Select', module)
+  .add(dogSelectRenderInfo.text, () => dogSelectRender, dogSelectRenderInfo.parameters)
+  .add(catSelectRenderInfo.text, () => catSelectRender, catSelectRenderInfo.parameters)
--- packages/client/src/pattern/atom/Select/__tests__/Select.int.test.js
+++ packages/client/src/pattern/atom/Select/__tests__/Select.int.test.js
@@ -1,9 +1,13 @@
-import { dogSelectRender } from './Select.int.render'
+import { dogSelectRender, catSelectRender } from './Select.int.render'
 
 describe('<Select />', () => {
   describe('Snaphot', () => {
     test('must match dogSelectRender', () => {
       expect(global.renderToJSON(dogSelectRender)).toMatchSnapshot()
+    })
+
+    test('must match catSelectRender', () => {
+      expect(global.renderToJSON(catSelectRender)).toMatchSnapshot()
     })
   })
 })

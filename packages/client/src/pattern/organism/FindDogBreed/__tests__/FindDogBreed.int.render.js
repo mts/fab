8050c3c7e1d5c16c49afb8cafb5033cc36d68a1e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-23 20:36:55 +0100

    test FindDogBreed

--- packages/client/src/pattern/organism/FindDogBreed/__tests__/FindDogBreed.int.render.js
+++ packages/client/src/pattern/organism/FindDogBreed/__tests__/FindDogBreed.int.render.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { FindDogBreed } from '../FindDogBreed'
+import { getAppMockStore } from '../../../../../../library/src/store/mock'
+import { mockAppState } from '../../../../../../library/src/state/mock'
 
-export const defaultRender = <FindDogBreed />
+export const defaultRender = (() => {
+  return (
+    <Provider store={getAppMockStore(mockAppState)}>
+      <FindDogBreed />
+    </Provider>
+  )
+})()

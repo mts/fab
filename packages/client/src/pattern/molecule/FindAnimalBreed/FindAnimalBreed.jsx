77a52afccfdaef333c1704fca703f55b85c552b1
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-23 13:28:25 +0100

    extend FindAnimalBreed with Select

--- packages/client/src/pattern/molecule/FindAnimalBreed/FindAnimalBreed.jsx
+++ packages/client/src/pattern/molecule/FindAnimalBreed/FindAnimalBreed.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import cx from 'classnames'
 import { findAnimalBreedDefaultProps, findAnimalBreedPropTypes } from './FindAnimalBreed.prop'
 import { breed } from './FindAnimalBreed.scss'
+import { Select } from '../../atom/Select/Select'
 
-export function FindAnimalBreed({ className }) {
-  return <div className={cx(className, breed)}>FindAnimalBreed under construction</div>
+export function FindAnimalBreed({ className, selectOptions }) {
+  return (
+    <div className={cx(className, breed)}>
+      <Select options={selectOptions} />
+    </div>
+  )
 }
 
 FindAnimalBreed.defaultProps = findAnimalBreedDefaultProps
--- packages/client/src/pattern/molecule/FindAnimalBreed/FindAnimalBreed.prop.js
+++ packages/client/src/pattern/molecule/FindAnimalBreed/FindAnimalBreed.prop.js
@@ -1,4 +1,5 @@
 import { string } from 'prop-types'
+import { selectPropTypes } from '../../atom/Select/Select.prop'
 
 export const findAnimalBreedDefaultProps = {
   className: '',
@@ -6,4 +7,5 @@ export const findAnimalBreedDefaultProps = {
 
 export const findAnimalBreedPropTypes = {
   className: string,
+  selectOptions: selectPropTypes.options,
 }

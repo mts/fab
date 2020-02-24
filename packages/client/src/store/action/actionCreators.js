11bbc1747646196316a404ece29654867dfcbcf2
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-24 20:02:30 +0100

    implement apiRestCatSetRandomImageAction()

--- packages/client/src/store/action/actionCreators.js
+++ packages/client/src/store/action/actionCreators.js
@@ -1,3 +1,4 @@
-import { API_REST_DOG_SET_RANDOM_IMAGE } from './actionTypes'
+import { API_REST_DOG_SET_RANDOM_IMAGE, API_REST_CAT_SET_RANDOM_IMAGE } from './actionTypes'
 
 export const apiRestDogSetRandomImageAction = payload => ({ type: API_REST_DOG_SET_RANDOM_IMAGE, payload })
+export const apiRestCatSetRandomImageAction = payload => ({ type: API_REST_CAT_SET_RANDOM_IMAGE, payload })

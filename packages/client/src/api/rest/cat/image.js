f172e7eb4efce2bc42c7d48a3a9108eab3cd4c52
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-24 21:46:06 +0100

    add api key

--- packages/client/src/api/rest/cat/image.js
+++ packages/client/src/api/rest/cat/image.js
@@ -1,6 +1,6 @@
 export const getRandomCatImageByBreed = async breed => {
-  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breed}`
-
+  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breed}&api_key=${process.env.CAT_API_KEY}`
   const response = await window.fetch(url)
+
   return response.json()
 }

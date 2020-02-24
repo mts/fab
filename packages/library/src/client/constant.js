6bfe5bd017385587646e1da3718f966f18af1e7f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-24 20:03:44 +0100

    extend constants with defaultBreed and httpResponseCode

--- packages/library/src/client/constant.js
+++ packages/library/src/client/constant.js
@@ -8,3 +8,12 @@ export const breedEntry = Object.freeze({
 export const keyCode = Object.freeze({
   enter: 13,
 })
+
+export const defaultBreed = Object.freeze({
+  dog: 'collie',
+  cat: 'beng',
+})
+
+export const httpResponseCode = Object.freeze({
+  notFound: 404,
+})

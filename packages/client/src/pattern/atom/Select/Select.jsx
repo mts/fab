87f2f0dfe359e2be2315c6bc5319097a9033f154
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-24 19:08:02 +0100

    remove Select className prop

--- packages/client/src/pattern/atom/Select/Select.jsx
+++ packages/client/src/pattern/atom/Select/Select.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import uuidv1 from 'uuid/v1'
-import cx from 'classnames'
 import { selectDefaultProps, selectPropTypes } from './Select.prop'
 import { select } from './Select.scss'
 import { clientAppStore } from '../../../store/client'
 import { setAppCompleteStateThunk } from '../../../store/thunk/completeState'
 
-export function Component({ className, options, randomImage }) {
+export function Component({ options, randomImage }) {
   function onChange(e) {
     clientAppStore.dispatch(setAppCompleteStateThunk(e.target.value.split('-')[0]))
   }
 
   return (
-    <select className={cx(className, select)} onChange={onChange} onBlur={() => {}}>
+    <select className={select} onChange={onChange} onBlur={() => {}}>
       {options.map(({ value, text }) => {
         return (
           <option selected={value === randomImage.subBreed} key={uuidv1()} value={value}>
--- packages/client/src/pattern/atom/Select/Select.prop.js
+++ packages/client/src/pattern/atom/Select/Select.prop.js
@@ -1,11 +1,8 @@
 import { string, shape } from 'prop-types'
 
-export const selectDefaultProps = {
-  className: '',
-}
+export const selectDefaultProps = {}
 
 export const selectPropTypes = {
-  className: string,
   options: shape({
     value: string.isRequired,
     text: string.isRequired,

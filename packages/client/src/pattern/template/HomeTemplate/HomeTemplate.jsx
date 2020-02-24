8cdee20c2063dab4f63428810d6041dfec90feed
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-24 19:26:26 +0100

    remove HomeTemplate unnecessary styling

--- packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
+++ packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import { homeTemplate } from './HomeTemplate.scss'
 import { homeTemplateDefaultProps, homeTemplatePropTypes } from './HomeTemplate.prop'
 import { FindDogBreed } from '../../organism/FindDogBreed'
 
 export function HomeTemplate() {
-  return (
-    <div className={homeTemplate}>
-      <FindDogBreed />
-    </div>
-  )
+  return <FindDogBreed />
 }
 
 HomeTemplate.defaultProps = homeTemplateDefaultProps

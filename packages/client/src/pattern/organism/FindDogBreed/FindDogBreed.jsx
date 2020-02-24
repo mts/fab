72e6802cbd1e833358cf8f730cedd8c6380a984a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-24 20:46:53 +0100

    pass setRandomImage() on to FindAnimalBreed

--- packages/client/src/pattern/organism/FindDogBreed/FindDogBreed.jsx
+++ packages/client/src/pattern/organism/FindDogBreed/FindDogBreed.jsx
@@ -6,14 +6,21 @@ import { findDogBreedDefaultProps, findDogBreedPropTypes } from './FindDogBreed.
 import { FindAnimalBreed } from '../../molecule/FindAnimalBreed'
 import { dogSelectOptions } from '../../atom/Select/__tests__/Select.int.render'
 import { breedEntry } from '../../../../../library/src/client/constant'
+import { clientAppStore } from '../../../store/client'
+import { setDogRandomImageThunk } from '../../../store/thunk/setRandomImage'
 
 export function Component({ randomImage }) {
+  function setRandomImage(breed) {
+    clientAppStore.dispatch(setDogRandomImageThunk(breed))
+  }
+
   return (
     <FindAnimalBreed
       searchEntry={breedEntry.input}
       selectOptions={dogSelectOptions}
       buttonText="Search Dog"
       imageSource={{ src: randomImage.src, alt: randomImage.alt }}
+      setRandomImage={setRandomImage}
     />
   )
 }

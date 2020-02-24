9fd44473efe5785a690036b055c79dbb8c8e5de7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-24 19:24:38 +0100

    remove NoMatch unnecessary div

--- packages/client/src/pattern/atom/NoMatch/NoMatch.jsx
+++ packages/client/src/pattern/atom/NoMatch/NoMatch.jsx
@@ -3,11 +3,9 @@ import { noMatchDefaultProps, noMatchPropTypes } from './NoMatch.prop'
 
 export function NoMatch({ location }) {
   return (
-    <div>
-      <h3>
-        No match for <code>{location.pathname}</code>
-      </h3>
-    </div>
+    <h3>
+      No match for <code>{location.pathname}</code>
+    </h3>
   )
 }
 

c76b99b91ab3b98b000b8615fb1ae98bf6edba64
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-24 20:03:17 +0100

    cover apiRestCatSetRandomImageAction() with tests

--- packages/client/src/store/action/__tests__/actionCreators.test.js
+++ packages/client/src/store/action/__tests__/actionCreators.test.js
@@ -1,4 +1,4 @@
-import { apiRestDogSetRandomImageAction } from '../actionCreators'
+import { apiRestDogSetRandomImageAction, apiRestCatSetRandomImageAction } from '../actionCreators'
 
 describe('actionCreators', () => {
   const payload = { somePayload: 'some-payload' }
@@ -6,6 +6,12 @@ describe('actionCreators', () => {
   describe('apiRestDogSetRandomImageAction()', () => {
     test('must return action with given payload', () => {
       expect(apiRestDogSetRandomImageAction(payload)).toMatchObject({ type: 'API_REST_DOG_SET_RANDOM_IMAGE', payload })
+    })
+  })
+
+  describe('apiRestCatSetRandomImageAction()', () => {
+    test('must return action with given payload', () => {
+      expect(apiRestCatSetRandomImageAction(payload)).toMatchObject({ type: 'API_REST_CAT_SET_RANDOM_IMAGE', payload })
     })
   })
 })

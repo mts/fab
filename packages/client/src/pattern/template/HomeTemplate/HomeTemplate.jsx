c8d81d40d97607b54337b1aab317508d5fb0d7c4
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-24 21:55:03 +0100

    render FindDogBreed and FindCatBreed alongside each other

--- packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
+++ packages/client/src/pattern/template/HomeTemplate/HomeTemplate.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { homeTemplateDefaultProps, homeTemplatePropTypes } from './HomeTemplate.prop'
 import { FindDogBreed } from '../../organism/FindDogBreed'
+import { FindCatBreed } from '../../organism/FindCatBreed'
+import { homeTemplate } from './HomeTemplate.scss'
 
 export function HomeTemplate() {
-  return <FindDogBreed />
+  return (
+    <div className={homeTemplate}>
+      <FindDogBreed />
+      <FindCatBreed />
+    </div>
+  )
 }
 
 HomeTemplate.defaultProps = homeTemplateDefaultProps

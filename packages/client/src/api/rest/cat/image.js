271d9d5b4c364990d0ff04d8e84159998b98a7ce
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-24 23:55:42 +0100

    use api key in request header

--- packages/client/src/api/rest/cat/image.js
+++ packages/client/src/api/rest/cat/image.js
@@ -1,6 +1,16 @@
 export const getRandomCatImageByBreed = async breed => {
-  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breed}&api_key=${process.env.CAT_API_KEY}`
-  const response = await window.fetch(url)
+  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breed}`
+
+  const getRequestOptions = {
+    headers: {
+      'content-type': 'application/json',
+      'x-api-key': process.env.CAT_API_KEY,
+    },
+    method: 'GET',
+    mode: 'cors',
+  }
+
+  const response = await window.fetch(url, getRequestOptions)
 
   return response.json()
 }

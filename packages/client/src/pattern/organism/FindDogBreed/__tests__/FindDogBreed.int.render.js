85ce3e3f2faaabb977a1110b7d240152d3129226
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-25 10:42:41 +0100

    complement FindDogBreed test

--- packages/client/src/pattern/organism/FindDogBreed/__tests__/FindDogBreed.int.render.js
+++ packages/client/src/pattern/organism/FindDogBreed/__tests__/FindDogBreed.int.render.js
@@ -3,11 +3,12 @@ import { Provider } from 'react-redux'
 import { FindDogBreed } from '../FindDogBreed'
 import { getAppMockStore } from '../../../../../../library/src/store/mock'
 import { mockAppState } from '../../../../../../library/src/state/mock'
+import { dogRandomImage } from '../../../atom/Image/__tests__/Image.int.render'
 
 export const defaultRender = (() => {
   return (
     <Provider store={getAppMockStore(mockAppState)}>
-      <FindDogBreed />
+      <FindDogBreed randomImage={dogRandomImage} searchBreed="some-search-breed" apiRestDogSetSearchBreedAction={() => {}} />
     </Provider>
   )
 })()

414259324bb05323c954d7a1d7da455228d68c88
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-25 10:31:11 +0100

    remove Select from FindAnimalBreed

--- packages/client/src/pattern/molecule/FindAnimalBreed/FindAnimalBreed.jsx
+++ packages/client/src/pattern/molecule/FindAnimalBreed/FindAnimalBreed.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
 import { findAnimalBreedDefaultProps, findAnimalBreedPropTypes } from './FindAnimalBreed.prop'
-import { breed, breedSearch, breedSearchSelect, breedSearchButton, breedShow, breedSearchInput } from './FindAnimalBreed.scss'
-import { Select } from '../../atom/Select'
+import { breed, breedSearch, breedSearchButton, breedShow, breedSearchInput } from './FindAnimalBreed.scss'
 import { Input } from '../../atom/Input'
 import { Button } from '../../atom/Button'
 import { Image } from '../../atom/Image'
-import { breedEntry } from '../../../../../library/src/client/constant'
 
-export function FindAnimalBreed({ searchEntry, selectOptions, buttonText, randomImage, setRandomImage, searchBreed, setSearchBreed }) {
+export function FindAnimalBreed({ buttonText, randomImage, setRandomImage, searchBreed, setSearchBreed }) {
   return (
     <div className={breed}>
       <h2>A random image of {randomImage.breed} breed</h2>
       <h4>Type in new breed, press enter or click search</h4>
       <div className={breedSearch}>
-        {searchEntry === breedEntry.select ? (
-          <Select className={breedSearchSelect} options={selectOptions} randomImage={randomImage} setRandomImage={setRandomImage} />
-        ) : (
-          <Input className={breedSearchInput} randomImage={randomImage} setRandomImage={setRandomImage} setSearchBreed={setSearchBreed} />
-        )}
+        {' '}
+        <Input className={breedSearchInput} randomImage={randomImage} setRandomImage={setRandomImage} setSearchBreed={setSearchBreed} />
         <Button className={breedSearchButton} text={buttonText} setRandomImage={setRandomImage} searchBreed={searchBreed} />
       </div>
       <div className={breedShow}>

4d8f84f3b6ac4fded46daaeb46eeb1207331ddf6
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-25 10:38:01 +0100

    change usage description

--- packages/client/src/pattern/molecule/FindAnimalBreed/FindAnimalBreed.jsx
+++ packages/client/src/pattern/molecule/FindAnimalBreed/FindAnimalBreed.jsx
@@ -9,7 +9,7 @@ export function FindAnimalBreed({ buttonText, randomImage, setRandomImage, searc
   return (
     <div className={breed}>
       <h2>A random image of {randomImage.breed} breed</h2>
-      <h4>Type in new breed, press enter or click search</h4>
+      <h4>Type in new breed then press enter or click search button</h4>
       <div className={breedSearch}>
         <Input className={breedSearchInput} randomImage={randomImage} setRandomImage={setRandomImage} setSearchBreed={setSearchBreed} />
         <Button className={breedSearchButton} text={buttonText} setRandomImage={setRandomImage} searchBreed={searchBreed} />

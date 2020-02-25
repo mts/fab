a5306be709790dded27ca2d5f975d21a94d906ad
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-25 10:34:11 +0100

    remove unused breedEntry constant

--- packages/library/src/client/constant.js
+++ packages/library/src/client/constant.js
@@ -1,10 +1,5 @@
 export const placeholderURL = Object.freeze('https://via.placeholder.com/300')
 
-export const breedEntry = Object.freeze({
-  select: 'select',
-  input: 'input',
-})
-
 export const keyCode = Object.freeze({
   enter: 13,
 })
